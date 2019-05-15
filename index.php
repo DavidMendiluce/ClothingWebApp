1983a934d317f6d5e047996104951b6a43d094eb
DavidMendiluce <davidmendiluce@gmail.com>
2019-05-15 22:15:07 +0200

    update index.php

--- index.php
+++ index.php
@@ -2,8 +2,11 @@
 <html>
 <meta name="viewport" content="width=device-width, initial-scale=1" />
 <head>
+<!--Bootstrap-->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 <title>Le Rousse</title>
 <link href="https://fonts.googleapis.com/css?family=Great+Vibes" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Lora" rel="stylesheet">
 <link rel="stylesheet" href="public/style.css">
 
 </head>
@@ -11,24 +14,48 @@
 <body>
 
 
-<header id="home">
-  <h1>Le Rousse</h1>
-  <div class="navBar">
-  <ul>
-    <li>Le Rousse</li>
-    <li>Ultima Semana</li>
-    <li>Coleccion</li>
-    <li>Calzado</li>
-    <li>Bolsos</li>
-    <li>Accesorios</li>
-    <form>
-      <input class="searcher" type="search" placeholder="search"></type>
-    </form>
-  </ul>
-</div>
+<header>
+  <nav class="navbar-light navbar-expand-sm">
+    <h1 class="text-center">Le Rousse</h1>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+    aria-support="navbarSupportedContent" aria-expanded="false" aria-label="toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+      <div class="collapse navbar-collapse bg-light" id="navbarSupportedContent">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Le Rousse</a>
+          </li>
+          <li class="nav-item" >
+            <a class="nav-link" href="#">Ultima Semana</a>
+            </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Coleccion</a>
+            </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Calzado</a>
+            </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Bolsos</a>
+            </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Accesorios</a>
+            </li>
+      </ul>
+      <form class="form-inline my-2 my-lg-0 ml-auto">
+        <img class="float-right" src="img/user.png" width="35vh" height="35vh">
+        <img class="float-right" src="img/cart.png" width="35vh" height="35vh">
+           <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+         </form>
+     </div>
+  </nav>
 
 </header>
-
+<!-- Bootstrap JavaScript Libraries -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</body>
 
 </footer>
 <script
