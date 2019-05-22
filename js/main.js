c96e91f17c70bb7ffda7914ad5f29892703ad773
DavidMendiluce <davidmendiluce@gmail.com>
2019-05-22 13:28:50 +0200

    update main.js

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+  $('#search').on('keydown', () => {
+    $(".fa-search").hide();
+  });
+
+  $('#search').on('focusout', () => {
+    $(".fa-search").show();
+  });
+
+  $('#user').on('click', () => {
+    $(".loginRow").toggle();
+  });
+
+  $('#user').on('click', () => {
+    $(".logedRow").toggle();
+  });
+
+});
